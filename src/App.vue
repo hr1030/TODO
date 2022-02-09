93a7dbeea0bf3dc0ba63d801a0ed2f3bc432ad5b
韩蕊 <15620803165@163.com>
2022-02-09 08:25:57 +0800

    创建vue项目

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!-- 3.使用组件标签 -->
+    <HelloWorld/>
+
+    <div class="container">
+      <Add/>
+      <List/>
+
+    </div>
+  </div>
+</template>
+
+<script>
+// 1.引入组件
+import HelloWorld from './components/HelloWorld'
+import Add from './components/add'
+import List from './components/list'
+
+
+export default {
+  data(){
+    return{
+      comments:[
+        {
+          name:'BOB',
+          content:'Vue还不错'
+        },
+        {
+          name:'Ym',
+          content:'Vue so easy'
+        },
+        {
+          name:'Marry',
+          content:'Vue还可以'
+        },
+
+      ]
+
+    }
+  },
+
+
+  name: 'App',
+  //2.映射组件标签
+  components: {
+    HelloWorld,
+    Add,
+    List
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
